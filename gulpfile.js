e77c7948706bde28e3eaa3872df4f01c60700e62
Jimmy Lindemann <jlindemann@engagesoftware.com>
2023-10-14 16:49:58 -0500

    compile less to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-var gulp = require("gulp");
-var elm = require("gulp-elm");
-
+const gulp = require("gulp");
+const elm = require("gulp-elm");
+const less = require("gulp-less");
+const plumber = require("gulp-plumber");
+//elm
 function elmBundle() {
   return gulp
     .src("Views/**/Main.elm")
@@ -9,4 +11,18 @@ function elmBundle() {
 }
 
 gulp.task("elm-bundle", elmBundle);
-exports.default = elmBundle;
+
+//less
+gulp.task("less", function () {
+  return gulp
+    .src("wwwroot/css/base.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulp.dest("wwwroot/css/"));
+});
+
+gulp.task("watch", function () {
+  gulp.watch("wwwroot/css/base.less", gulp.series("less"));
+});
+
+gulp.task("default", gulp.series("elm-bundle", "less", "watch"));
