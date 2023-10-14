38bfef0ed0ed2e2df1218c106486cf2235890228
Cregg Hancock <creggh@gmail.com>
2023-10-14 15:21:13 -0500

    Add mock character sheet markup to elm

---
+++ Controllers/CharacterSheetController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DndManager.Controllers;
+
+public class CharacterSheetController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
